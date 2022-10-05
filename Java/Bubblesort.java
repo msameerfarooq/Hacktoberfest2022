812d9d291a081ee3f2d1e87d51c4edac396c261f
Anand <73114463+Anand4756@users.noreply.github.com>
2022-10-05 17:01:05 +0530

    added bubblesort program in java

---
+++ Java/Bubblesort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Bubblesort {  
+    static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }  
+    public static void main(String[] args) {  
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int arr[] = new int[x];
+             for(int i=0;i<x;i++){
+				 arr[i] = sc.nextInt();
+			 }
+                bubbleSort(arr);
+                 
+                System.out.println("Array after Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+   
+        }  
+}  
