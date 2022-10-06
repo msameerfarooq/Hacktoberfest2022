dfd8504166400e9a006ad3cb4d66ac9e13b24576
Shreyasheeetal20 <87710926+Shreyasheeetal20@users.noreply.github.com>
2022-10-06 12:35:00 +0530

    Create MinMaxElementsOfArray.java
    Signed-off-by: Shreyasheeetal20 <87710926+Shreyasheeetal20@users.noreply.github.com>

---
+++ 05. Arrays/MinMaxElementsOfArray.java
@@ -0,0 +1,19 @@
+public class MinMaxElementsofArray {
+    public static void main(String[] args) {
+        int [] nums = {12,9,5,76,3,42,76,22};
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if (nums[i]>max){
+                max=nums[i];
+            }
+        }
+        System.out.println("Max is "+max);
+        for(int i=0; i<nums.length; i++){
+            if (nums[i]<min){
+                min=nums[i];
+            }
+        }
+        System.out.println("Min is "+min);
+    }
+}
