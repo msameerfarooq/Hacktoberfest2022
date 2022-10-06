6be8140b7225c73d274b43bff859e1323fa6f1fb
Hamza Hasan <114472293+hhasan001@users.noreply.github.com>
2022-10-06 22:00:43 +0530

    Create Taylor_Series.cpp
    Signed-off-by: Hamza Hasan <114472293+hhasan001@users.noreply.github.com>

---
+++ 04. Recursion/Taylor_Series.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+double e(int x, int n)
+{
+ static double p=1,f=1;
+ double r;
+
+ if(n==0)
+ return 1;
+ r=e(x,n-1);
+ p=p*x;
+ f=f*n;
+ return r+p/f;
+}
+int main()
+{
+ cout<<e(4,15)<<endl;
+ return 0;
+}
