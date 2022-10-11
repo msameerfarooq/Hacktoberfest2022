df602868f7142453d9d234bf89b77621e37f3d6b
ZoolfWoolf <zulfiqarchkonoha@gmail.com>
2022-10-11 23:40:01 +0500

    Fixed InsertionSort

--- Java/InsertionSort.java
+++ Java/InsertionSort.java
@@ -1,8 +1,10 @@
-public class InsertionSort{
-  public static void main(String[] args){
+import java.util.Arrays;
 
-  System.out.println(Arrays.toString(new int[]{8,4,2,1,6}))
-     
+public class InsertionSort {
+  public static void main(String[] args) {
+
+    System.out.println(Arrays.toString(new int[] { 8, 4, 2, 1, 6 }));
+
   }
 
   static int[] insertionSort(int[] array) {
@@ -10,12 +12,12 @@ public class InsertionSort{
       int current = array[i];
       int j = i - 1;
       while (j >= 0 && array[j] > current) {
-          array[j + 1] = array[j];
-          j--;
-        }
-        array[j + 1] = current;
+        array[j + 1] = array[j];
+        j--;
       }
-    return array;
+      array[j + 1] = current;
     }
+    return array;
+  }
 
 }
