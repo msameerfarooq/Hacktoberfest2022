39d444d660204b58f229d196cb5ee390e37f629c
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 22:53:08 +0530

    Update 5. Nth_catalan_number.cpp
    Signed-off-by: Aman Bhavsar <96308303+5264A@users.noreply.github.com>

--- 02. Mathematics/5. Nth_catalan_number.cpp
+++ 02. Mathematics/5. Nth_catalan_number.cpp
@@ -6,21 +6,29 @@ using namespace std;
     Constraints 1<=n<=19
 
 */
-long catalan(int n)
-{
-    long ar[n + 1];
- 
-    ar[0] = 1;
-    ar[1] = 1;
+cpp_int findCatalan(int n) 
+
+    {
+
+        cpp_int dp[n+1];
+
+        dp[0]=1;
+
+        dp[1]=1;
+
+        for(int i=2; i<n+1; i++){
+
+            for(int j=0; j<i; j++){
+
+                dp[i]+=dp[j]*dp[i-j-1];
+
+            }
+
+        }
+
+        return dp[n];
+
     
-    for (int i = 2; i <= n; i++) {
-        ar[i] = 0;
-        for (int j = 0; j < i; j++)
-            ar[i] += ar[j] * ar[i - j - 1];
-    }
- 
-   
-    return ar[n];
 }
  
 int main()
@@ -28,6 +36,6 @@ int main()
     int n;
     cout<<"Enter the number ";
     cin>>n;
-    cout<<"\n"<<n<<"th Catalan number is:"<<catalan(n)<<"\n";
+    cout<<"\n"<<n<<"th Catalan number is:"<<findCatalan(n)<<"\n";
     return 0;
 }
