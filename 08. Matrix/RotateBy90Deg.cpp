9a32bab84fedad531119a9e3fd05563945e787cc
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 22:22:29 +0530

    Update RotateBy90Deg.cpp
    Please accept it i write by soending 1 hour on it Please accept this pr>....
    
    Signed-off-by: Aman Bhavsar <96308303+5264A@users.noreply.github.com>

--- 08. Matrix/RotateBy90Deg.cpp
+++ 08. Matrix/RotateBy90Deg.cpp
@@ -1,26 +1,91 @@
-#include<bits/stdc++.h>
+// C++ implementation of above approach
+#include <bits/stdc++.h>
 using namespace std;
-vector < vector < int >> rotate(vector < vector < int >> & matrix) {
-  int n = matrix.size();
-  vector < vector < int >> rotated(n, vector < int > (n, 0));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      rotated[j][n - i - 1] = matrix[i][j];
+ 
+#define N 4
+ 
+// Function to rotate the matrix 90 degree clockwise
+void rotate90Clockwise(int a[N][N])
+{
+ 
+    // Traverse each cycle
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = i; j < N - i - 1; j++) {
+ 
+            // Swap elements of each cycle
+            // in clockwise direction
+            int temp = a[i][j];
+            a[i][j] = a[N - 1 - j][i];
+            a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
+            a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
+            a[j][N - 1 - i] = temp;
+        }
     }
-  }
-  return rotated;
 }
-
-int main() {
-  vector < vector < int >> arr;
-  arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  vector < vector < int >> rotated = rotate(arr);
-  cout << "Rotated Image" << endl;
-  for (int i = 0; i < rotated.size(); i++) {
-    for (int j = 0; j < rotated[0].size(); j++) {
-      cout << rotated[i][j] << " ";
+ 
+// Function for print matrix
+void printMatrix(int arr[N][N])
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << arr[i][j] << " ";
+        cout << '\n';
     }
-    cout << "\n";
-  }
-
+}
+ 
+// Driver code
+int main()
+{
+    int arr[N][N] = { { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 },
+                      { 9, 10, 11, 12 },
+                      { 13, 14, 15, 16 } };
+    rotate90Clockwise(arr);
+    printMatrix(arr);
+    return 0;
+}// C++ implementation of above approach
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define N 4
+ 
+// Function to rotate the matrix 90 degree clockwise
+void rotate90Clockwise(int a[N][N])
+{
+ 
+    // Traverse each cycle
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = i; j < N - i - 1; j++) {
+ 
+            // Swap elements of each cycle
+            // in clockwise direction
+            int temp = a[i][j];
+            a[i][j] = a[N - 1 - j][i];
+            a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
+            a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
+            a[j][N - 1 - i] = temp;
+        }
+    }
+}
+ 
+// Function for print matrix
+void printMatrix(int arr[N][N])
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << arr[i][j] << " ";
+        cout << '\n';
+    }
+}
+ 
+// Driver code
+int main()
+{
+    int arr[N][N] = { { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 },
+                      { 9, 10, 11, 12 },
+                      { 13, 14, 15, 16 } };
+    rotate90Clockwise(arr);
+    printMatrix(arr);
+    return 0;
 }
