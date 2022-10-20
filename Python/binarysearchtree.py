b666ea705172fab52124a0c6c23342a3a48b7a81
Muhammad Sameer Farooq <me.sameer.farooq@gmail.com>
2022-10-20 10:49:09 +0500

    Update binarysearchtree.py
    Signed-off-by: Muhammad Sameer Farooq <me.sameer.farooq@gmail.com>

--- Python/binarysearchtree.py
+++ Python/binarysearchtree.py
@@ -25,12 +25,8 @@ def binary_search(arr, low, high, x):
 
 # Test array
 arr = list(map(int, input().split()))
-x = int(input())
 
-# Function call
-result = binary_search(arr, 0, len(arr) - 1, x)
-
-if result != -1:
+if binary_search(arr, 0, len(arr) - 1, int(input())) != -1:
     print("Element is present at index", str(result))
 else:
     print("Element is not present in array")
