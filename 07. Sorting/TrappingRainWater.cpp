10195e021913856cac2cf28484b504558967650d
MidhaShrey <midhashrey@gmail.com>
2022-10-25 16:14:55 +0530

    Added my code sample for issue #247

---
+++ 07. Sorting/TrappingRainWater.cpp
@@ -0,0 +1,51 @@
+#include <Iostream>
+using namespace std;
+
+int MaxWater(int arr[], int n)
+{
+	int size = n - 1;
+
+	int prev = arr[0];
+
+	
+	int prev_index = 0;
+	int water = 0;
+	int temp = 0;
+	for (int i = 1; i <= size; i++) {
+		if (arr[i] >= prev) {
+			prev = arr[i];
+			prev_index = i;
+			temp = 0;
+		}
+		else {
+			water += prev - arr[i];
+			temp += prev - arr[i];
+		}
+	}
+
+	if (prev_index < size) {
+
+		water -= temp;
+		prev = arr[size];
+		for (int i = size; i >= prev_index; i--) {
+			if (arr[i] >= prev) {
+				prev = arr[i];
+			}
+			else {
+				water += prev - arr[i];
+			}
+		}
+	}
+
+	return water;
+}
+
+int main()
+{
+	int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	cout << MaxWater(arr, N);
+	return 0;
+}
+
