7d3642045af701606e7daa8d22324025665941bc
Arnav Garg <78140728+Arnav2982@users.noreply.github.com>
2022-10-25 09:11:41 +0530

    Update FibonacciSearch.cpp
    Signed-off-by: Arnav Garg <78140728+Arnav2982@users.noreply.github.com>

--- 06. Searching/FibonacciSearch.cpp
+++ 06. Searching/FibonacciSearch.cpp
@@ -1,49 +1,77 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-//fibonacci search of elemet on sorted array arr of size n
-//iterative approach
-//time : O(log(n))
-//space : O(1) 
-//f(n)~c*1.62^n
-//O(log₁.₆₂(n)) not better than Binary search O(log₂(n))
-int FibonacciSearch(int arr[], int n, int element){
-    int f2=0;
-    int f1=1;
-    int f0=f2+f1;
-
-    while(f0<n){
-        f2=f1;
-        f1=f0;
-        f0=f1+f2;
+ 
+// Utility function to find minimum of two elements
+int min(int x, int y) { return (x <= y) ? x : y; }
+  
+/* Returns index of x if present,  else returns -1 */
+int fibMonaccianSearch(int arr[], int x, int n)
+{
+    /* Initialize fibonacci numbers */
+    int fibMMm2 = 0; // (m-2)'th Fibonacci No.
+    int fibMMm1 = 1; // (m-1)'th Fibonacci No.
+    int fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
+  
+    /* fibM is going to store the smallest Fibonacci
+       Number greater than or equal to n */
+    while (fibM < n) {
+        fibMMm2 = fibMMm1;
+        fibMMm1 = fibM;
+        fibM = fibMMm2 + fibMMm1;
     }
-    int offset=-1;
-    while(f0>1){
-        int i = min(offset+f2,n-1);
-        if(arr[i]<element){
-            f0=f1;
-            f2=f0-f2;
-            f1=f0-f2;
-            offset=i;
+  
+    // Marks the eliminated range from front
+    int offset = -1;
+  
+    /* while there are elements to be inspected. Note that
+       we compare arr[fibMm2] with x. When fibM becomes 1,
+       fibMm2 becomes 0 */
+    while (fibM > 1) {
+        // Check if fibMm2 is a valid location
+        int i = min(offset + fibMMm2, n - 1);
+  
+        /* If x is greater than the value at index fibMm2,
+           cut the subarray array from offset to i */
+        if (arr[i] < x) {
+            fibM = fibMMm1;
+            fibMMm1 = fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+            offset = i;
         }
-        else if(arr[i]>element){
-            f0=f2;
-            f1=f1-f2;
-            f2=f0-f1;
+  
+        /* If x is greater than the value at index fibMm2,
+           cut the subarray after i+1  */
+        else if (arr[i] > x) {
+            fibM = fibMMm2;
+            fibMMm1 = fibMMm1 - fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
         }
-        else return i;
+  
+        /* element found. return index */
+        else
+            return i;
     }
-    if (arr[n-1] == element)
-            return n-1;
+  
+    /* comparing the last element with x */
+    if (fibMMm1 && arr[offset + 1] == x)
+        return offset + 1;
+  
+    /*element not found. return -1 */
     return -1;
 }
-int main(){
-    int arr[]={0,1,2,3,4,5,6,7,8,9,10,11,12};
-    int n=sizeof(arr)/sizeof(int);
-    for (int i=0;i<=n;i++)
-    {
-        if(i!=FibonacciSearch(arr,n,i))
-            cout<<"failled at "<<i<<" with output "<<FibonacciSearch(arr,i,n)<<endl;
-    }
-    cout<<endl;
+ 
+// Driver Code
+int main()
+{
+    int arr[]
+        = { 10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100,235};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 235;
+      int ind = fibMonaccianSearch(arr, x, n);
+  if(ind>=0)
+    cout << "Found at index: " << ind;
+  else
+    cout << x << " isn't present in the array";
+ 
+    return 0;
 }
