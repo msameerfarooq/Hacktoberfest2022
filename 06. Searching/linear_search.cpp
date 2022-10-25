1e0d48796ec19f5e7f8c0e576a4541e9827a4abc
Arnav Garg <78140728+Arnav2982@users.noreply.github.com>
2022-10-25 09:13:35 +0530

    Update linear_search.cpp
    Signed-off-by: Arnav Garg <78140728+Arnav2982@users.noreply.github.com>

--- 06. Searching/linear_search.cpp
+++ 06. Searching/linear_search.cpp
@@ -1,27 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int search(int arr[], int n, int x)
+#include <stdio.h>
+ 
+int search(int arr[], int N, int x)
 {
-	int i;
-	for (i = 0; i < n; i++)
-		if (arr[i] == x)
-			return i;
-	return -1;
+    int i;
+    for (i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
 }
-
-// Driver code
-int main()
+ 
+// Driver's code
+int main(void)
 {
-	int arr[] = { 3, 4, 1, 7, 5 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int x = 4;
-
-	int index = search(arr, n, x);
-	if (index == -1)
-		cout << "Element is not present in the array";
-	else
-		cout << "Element found at position " << index;
-
-	return 0;
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int N = sizeof(arr) / sizeof(arr[0]);
+ 
+    // Function call
+    int result = search(arr, N, x);
+    (result == -1)
+        ? printf("Element is not present in array")
+        : printf("Element is present at index %d", result);
+    return 0;
 }
