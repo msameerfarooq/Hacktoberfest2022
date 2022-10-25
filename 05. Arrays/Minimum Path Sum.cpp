dbb53dc0cbe5237698ec00d856247be52234266d
Arnav Garg <78140728+Arnav2982@users.noreply.github.com>
2022-10-25 09:04:30 +0530

    Update Minimum Path Sum.cpp
    Signed-off-by: Arnav Garg <78140728+Arnav2982@users.noreply.github.com>

--- 05. Arrays/Minimum Path Sum.cpp
+++ 05. Arrays/Minimum Path Sum.cpp
@@ -1,46 +1,73 @@
-
 #include <iostream>
-#include<bits/stdc++.h>
 using namespace std;
-
-
-vector<vector<int>> dp;
-int minPathSum(vector<vector<int>>& grid) {
-    int n = grid.size(), m = grid[0].size();
-    vector<vector<int>> dp(n, vector<int> (m, 0));
-        
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==0 && j==0)
-                dp[i][j] = grid[i][j];
-                
-            else{
-                int u = grid[i][j];
-                if(i>0)
-                    u += dp[i-1][j];
-                    
-                else
-                    u += 1e9;
-                
-                int l = grid[i][j];
-                if(j>0)
-                    l += dp[i][j-1];
-                else l += 1e9;
-                    
-                dp[i][j] = min(u, l);
-            }
+ 
+// Utility function to find maximum of two integers
+int max(int x, int y) { return (x > y) ? x : y; }
+ 
+// This function returns the sum of elements on maximum path
+// from beginning to end
+int maxPathSum(int ar1[], int ar2[], int m, int n)
+{
+    // initialize indexes for ar1[] and ar2[]
+    int i = 0, j = 0;
+ 
+    // Initialize result and current sum through ar1[] and
+    // ar2[].
+    int result = 0, sum1 = 0, sum2 = 0;
+ 
+    // Below 3 loops are similar to merge in merge sort
+    while (i < m && j < n) {
+        // Add elements of ar1[] to sum1
+        if (ar1[i] < ar2[j])
+            sum1 += ar1[i++];
+ 
+        // Add elements of ar2[] to sum2
+        else if (ar1[i] > ar2[j])
+            sum2 += ar2[j++];
+ 
+        else // we reached a common point
+        {
+            // Take the maximum of two sums and add to
+            // result
+            // Also add the common element of array, once
+            result += max(sum1, sum2) + ar1[i];
+ 
+            // Update sum1 and sum2 for elements after this
+            // intersection point
+            sum1 = 0;
+            sum2 = 0;
+ 
+            // update i and j to move to next element of
+            // each array
+            i++;
+            j++;
         }
     }
-        
-    return dp[n-1][m-1];
+ 
+    // Add remaining elements of ar1[]
+    while (i < m)
+        sum1 += ar1[i++];
+ 
+    // Add remaining elements of ar2[]
+    while (j < n)
+        sum2 += ar2[j++];
+ 
+    // Add maximum of two sums of remaining elements
+    result += max(sum1, sum2);
+ 
+    return result;
 }
-
+ 
+// Driver code
 int main()
 {
-    vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}};
-    dp.resize(3, vector<int> (3, 0));
-    
-    int r = minPathSum(grid);
-    cout << r;
-
+    int ar1[] = { 2, 3, 7, 10, 12, 15, 30, 34 };
+    int ar2[] = { 1, 5, 7, 8, 10, 15, 16, 19 };
+    int m = sizeof(ar1) / sizeof(ar1[0]);
+    int n = sizeof(ar2) / sizeof(ar2[0]);
+ 
+    // Function call
+    cout << "Maximum sum path is "
+         << maxPathSum(ar1, ar2, m, n);
+    return 0;
 }
